85e8727d1b72a870bff73160586202ad7bc740df
tkostas15 <56629679+tkostas15@users.noreply.github.com>
2022-06-09 12:48:52 +0300

    [ADDED] jest coverage and config in package.json

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,10 @@
-{
-  setupFilesAfterEnv: ["<rootDir>/setupTests.js"];
-}
+module.exports = {
+  setupFilesAfterEnv: ["<rootDir>/src/setupTests.js"],
+  collectCoverage: true,
+  collectCoverageFrom: ["<rootDir>/src/pages/*.{ts,js,tsx,jsx}"],
+  coverageDirectory: "<rootDir>/src/pages/__tests__/coverage",
+  testPathIgnorePatterns: [
+    "<rootDir>/src/pages/__tests__/exclude",
+    "<rootDir>/src/pages/__tests__/coverage",
+  ],
+};
